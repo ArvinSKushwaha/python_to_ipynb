35aea12b2ce4452f6651b25fb5227c00d3688687
Arvin Kushwaha <arvin.singh.kushwaha@gmail.com>
2023-10-18 04:02:36 -0400

    Added multi-language support, added README with example.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use argh::FromArgs;
 use ipynb_parse::{
-    Author, CellType, KernelSpecification, Metadata, Notebook, NotebookCell, NotebookMetadata,
+    Author, CellType, KernelSpecification, LanguageInfo, Metadata, Notebook, NotebookCell,
+    NotebookMetadata,
 };
 use std::{
     fs::File,
@@ -8,22 +9,94 @@ use std::{
     path::PathBuf,
     str::FromStr,
 };
+use thiserror::Error;
 
 /// Convert a python script to an ipynb
 #[derive(FromArgs)]
 pub struct Python2Ipynb {
     /// output path
-    #[argh(option)]
+    #[argh(option, short = 'o')]
     output_path: Option<PathBuf>,
 
     /// input path
     #[argh(positional)]
     input_path: PathBuf,
+
+    /// language: `python` or `julia`
+    #[argh(option, default = "Language::Python")]
+    language: Language,
+
+    /// list of authors
+    #[argh(positional, greedy)]
+    authors: Vec<String>,
+}
+
+#[derive(Default, PartialEq, Eq)]
+pub enum Language {
+    #[default]
+    Python,
+    Julia,
+}
+
+#[derive(Debug, Error)]
+#[error("Attempted to parse {attempted}, but expected 'julia' or 'python'")]
+pub struct LanguageParseError {
+    attempted: String,
+}
+
+impl FromStr for Language {
+    type Err = LanguageParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "python" => Ok(Language::Python),
+            "julia" => Ok(Language::Julia),
+            _ => Err(LanguageParseError {
+                attempted: s.to_string(),
+            }),
+        }
+    }
+}
+
+impl ToString for Language {
+    fn to_string(&self) -> String {
+        self.name().to_string()
+    }
+}
+
+impl Language {
+    fn info(&self) -> LanguageInfo {
+        LanguageInfo {
+            file_extension: self.file_extension(),
+            mimetype: self.mimetype(),
+            name: self.name(),
+        }
+    }
+
+    fn file_extension(&self) -> &str {
+        match self {
+            Language::Python => ".py",
+            Language::Julia => ".jl",
+        }
+    }
+
+    fn mimetype(&self) -> &str {
+        match self {
+            Language::Python => "application/python",
+            Language::Julia => "application/julia",
+        }
+    }
+
+    fn name(&self) -> &str {
+        match self {
+            Language::Python => "python",
+            Language::Julia => "julia",
+        }
+    }
 }
 
 fn main() -> anyhow::Result<()> {
     let args: Python2Ipynb = argh::from_env();
-    // The line #%% is special
 
     let input_path = args.input_path;
     let output_path = args.output_path.unwrap_or(
@@ -43,15 +116,18 @@ fn main() -> anyhow::Result<()> {
     let mut notebook = Notebook {
         metadata: NotebookMetadata {
             kernel_spec: KernelSpecification {
-                argv: vec!["python", "-m", "IPython.kernel", "-f", "{connection_file}"],
+                argv: None,
                 display_name: "python3",
-                language: "python",
+                language: &args.language.to_string(),
                 interrupt_mode: None,
                 env: None,
             },
-            authors: vec![Author {
-                name: "Arvin Kushwaha",
-            }],
+            language_info: Some(args.language.info()),
+            authors: args
+                .authors
+                .into_iter()
+                .map(|name| Author { name })
+                .collect(),
         },
         nbformat: 4,
         nbformat_minor: 4,
@@ -70,7 +146,7 @@ fn main() -> anyhow::Result<()> {
             if let Some(cell_type) = cell_type {
                 notebook.cells.push(NotebookCell {
                     cell_type,
-                    source: sources.join("\n"),
+                    source: sources.join("\n").trim_matches('\n').to_string(),
                     metadata: Metadata {
                         ..Default::default()
                     },
@@ -83,7 +159,7 @@ fn main() -> anyhow::Result<()> {
             current_cell_type = CellType::from_str(cell_type).ok();
         } else {
             let line = if current_cell_type != Some(CellType::Code) && line.starts_with("# ") {
-                line.split_at(2).1.to_owned()
+                line.split_at(2).1.to_string()
             } else {
                 line
             };
